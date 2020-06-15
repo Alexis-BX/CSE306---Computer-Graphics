306cac5d09e30dc7bdbabec60d1d592b9759685d
Alexis-BX <45032998+Zovj2auM@users.noreply.github.com>
2020-06-15 20:59:52 +0200

    lab 9

--- testVoronoi.cpp
+++ testVoronoi.cpp
@@ -4,9 +4,39 @@ double rand01(){
     return double(rand()) / double(RAND_MAX);
 }
 
+std::vector<Vector> tutte(std::vector<Vector> points, std::vector<std::vector<int>> adjcancy, std::vector<int> boundaryIndex) {
+    int n = boundaryIndex.size();
+    
+    double s = 0;
+    for (int i=0; i<n; i++)
+        s += norm(points[boundaryIndex[i]] - points[boundaryIndex[(i+1)<n?i+1:0]]);
+    
+    double cs = 0;
+    std::vector<Vector> v;
+
+    for (int i=0; i<n; i++){
+        double teta = 2*PI*cs/s;
+        points[boundaryIndex[i]] = Vector(cos(teta), sin(teta), 0);
+        cs += norm(points[boundaryIndex[i]] - points[boundaryIndex[(i+1)<n?i+1:0]]);
+    }
+
+    for (int j=0; j<100; j++){
+        std::vector<Vector> tmp = points;
+        for(int i = 0; i < n; i ++) {
+            tmp[i] = NULLVEC;
+            for(int k=0; k<adjcancy[i].size(); k++) 
+                tmp[i] += points[adjcancy[i][k]];
+            tmp[i] = tmp[i] / double(adjcancy[i].size());
+        } 
+        for(int i=0; i<n; i++) tmp[boundaryIndex[i]] = points[boundaryIndex[i]];
+        points = tmp;
+    }
+    return points;
+}
+
 int main(int argc, char *argv[]){
     
-    int N = 3;
+    int N = 10;
     std::vector<Vector> points(N);
     for(int i = 0; i < N; i ++) {
         points[i] = Vector(rand01(), rand01(), 0);
--- voronoi.cpp
+++ voronoi.cpp
@@ -7,18 +7,13 @@ public:
     Polygon(std::vector<Vector> points){
         p = points;
     }
-    Vector& operator[](int i) {return (i>0)?p[i%p.size()]:p[p.size()+i%p.size()];}  
-    const Vector& operator[](int i) const {return (i>0)?p[i%p.size()]:p[p.size()+i%p.size()];}
+    Vector& operator[](int i) {return p[i];}  
+    const Vector& operator[](int i) const {return p[i];}
     const unsigned int size() {return p.size();}
+    void add(Vector tmp) {p.push_back(tmp);}
 };
 
 std::vector<Polygon> vple(std::vector<Vector> points){
-    std::vector<Polygon> set(points.size());
-    if (points.size()==1) {
-        set[0] = Polygon(points);
-        return set;
-    }
-
     Vector boxCornerMin = points[0];
 	Vector boxCornerMax = points[0];
     for(int i=1; i<int(points.size()); i++){
@@ -29,110 +24,48 @@ std::vector<Polygon> vple(std::vector<Vector> points){
                                Vector(boxCornerMin[0], boxCornerMax[1], 0), 
                                boxCornerMax,
                                Vector(boxCornerMax[0], boxCornerMin[1], 0)};
-    tmp = {Vector(0, 0, 0), Vector(1, 0, 0), Vector(1, 1, 0), Vector(0, 1, 0)};
-
     Polygon boundingBox(tmp);
-    double e = 10e-6;
+
+    const double e = 10e-6;
+    std::vector<Polygon> set(points.size());
     
     #pragma omp parallel for
-    for(int i = 0; i < int(points.size()); i++) {
-        Vector center = points[i];
-        Polygon cell = boundingBox.p;
-        
-        for(int j = 0; j < int(points.size()); j++) {
-            if(i == j) continue;
+    for(int ic = 0; ic < points.size(); ic++) {
+        Vector center = points[ic];
+        Polygon cell = boundingBox;
 
-            Vector N = normalize(points[j] - center);
-            double t = dot(center + ((points[j] - center) / 2), N);
+        for(int ip = 0; ip < points.size(); ip++) {
+            if(ic == ip) continue;
+
+            Vector point = points[ip];
+
+            Vector N = normalize(point - center);
+            double t = dot(center + ((point - center) / 2), N);
 
             int n = cell.size();
             std::vector<double> ts(n);
-            for(int k = 0; k < n; k ++) ts[k] = dot(N, cell[k]) - t;
+            for(int i = 0; i < n; i ++) ts[i] = dot(N, cell[i]) - t;
+
             Polygon ncell;
-            for(int k = 0; k < n; k ++) {
-                int l = (k+1)%n;
+            for(int i = 0; i < n; i ++) {
+                int j = (i+1)%n;
                 int inter = 0;
-                if(ts[k] < e) {
-                    ncell.p.push_back(cell[k]);
-                    if(ts[l] > -e) inter = 1;
-                } else if(ts[l] < e) { inter = -1;}
-                    
-                if(inter!=0) {
-                    Vector v = cell[l] - cell[k];
-                    double abN = dot(v, N);
-                    Vector x;
-                    if(std::abs(abN) < e) {
-                        if(inter == 1) x = cell[l];
-                        else x = cell[k];
-                    } else {
-                        x = cell[k] + v * (t - dot(cell[k], N)) / abN;
-                    }
-                    ncell.p.push_back(x);
+                if(ts[i] < e) {
+                    ncell.add(cell[i]);
+                    if(ts[j] > -e) 
+                        inter = 1;
+                } else if(ts[j] < e) 
+                    inter = -1;
+
+                if(inter) {
+                    double ijN = dot((cell[j] - cell[i]), N);
+                    ncell.add((std::abs(ijN) < e)? cell[(inter==1)?j:i] : cell[i]+(cell[j]-cell[i])*(t-dot(cell[i],N))/ijN);
                 }
             }
             cell = ncell;
-            for(int k = 0; k < cell.size(); k ++) cell[k][2] = 0;
         }
-        set[i] = cell;
+        set[ic] = cell;
     }
     return set;
-    ///////////////////////////
-    /*std::vector<Vector> tmp = {Vector(0, 0, 0), Vector(1, 0, 0), Vector(1, 1, 0), Vector(0, 1, 0)};
 
-    Polygon boundingBox(tmp);
-    double M = 0;
-    double EPS = 10e-6;
-    for(auto point : points) M = std::max(point[2], M);
-    for(auto point : points) point[2] = sqrt(M - point[2]);
-    std::vector<Polygon> voronois(points.size());
-
-    for(int iC = 0; iC < int(points.size()); iC++) {
-        Vector C = points[iC];
-        std::vector<Vector> cell = boundingBox.p;
-
-        for(int iP = 0; iP < int(points.size()); iP++) {
-            if(iP == iC) continue;
-
-            Vector P = points[iP];
-            Vector CP = P - C;
-
-            Vector N = normalize(CP);
-            double T = dot(C + (CP / 2), N);
-
-            int n = cell.size();
-            std::vector<double> ts(n);
-            for(int i = 0; i < n; i ++) ts[i] = dot(N, cell[i]) - T;
-
-            std::vector<Vector> ncell;
-            for(int i = 0; i < n; i ++) {
-                int j = (i+1) % n;
-                int inter = 0;
-                if(ts[i] < EPS) {
-                    ncell.push_back(cell[i]);
-                    if(ts[j] > -EPS) inter = 1;
-                } else if(ts[j] < EPS) inter = 2;
-                if(inter) {
-                    Vector a = cell[i];
-                    Vector b = cell[j];
-                    Vector ab = b - a;
-                    double abN = dot(ab, N);
-                    Vector x;
-                    if(std::abs(abN) < EPS) {
-                        if(inter == 1) x = b;
-                        else x = a;
-                    } else {
-                        double t = (T - dot(a, N)) / abN;
-                        x = a + ab * t;
-                    }
-                    ncell.push_back(x);
-                }
-            }
-            cell = ncell;
-            for(int i = 0; i < n; i ++) cell[i][2] = 0;
-        }
-        voronois[iC].p = cell;
-    }
-
-    return voronois;
-*/
 }
